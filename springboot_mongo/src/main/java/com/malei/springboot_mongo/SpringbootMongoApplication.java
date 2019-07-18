7bd60234ee44a0a8c2e4fc64d3bd6a4faf6032d5
8143 <16619975586@163.com>
2019-07-18 17:41:04 +0800

    Initial commit

---
+++ springboot_mongo/src/main/java/com/malei/springboot_mongo/SpringbootMongoApplication.java
@@ -0,0 +1,13 @@
+package com.malei.springboot_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMongoApplication.class, args);
+    }
+
+}
---
+++ springboot_mongo/src/test/java/com/malei/springboot_mongo/SpringbootMongoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.malei.springboot_mongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootMongoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
